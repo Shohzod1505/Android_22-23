6d4a8afeff83d58866fba3653421dee2aa8f9e6a
Shohzod1505 <1505shakhzod@gmail.com>
2023-02-26 21:23:18 +0300

    new init

--- app/src/main/java/ru/itis/kpfu/homework/MainActivity.kt
+++ app/src/main/java/ru/itis/kpfu/homework/MainActivity.kt
@@ -2,40 +2,21 @@ package ru.itis.kpfu.homework
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.View
 import ru.itis.kpfu.homework.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private var _binding: ActivityMainBinding? = null
-    private val binding get() = _binding!!
+    private var binding: ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.title = "Shohzod1505"
-
-        var status = false
-        with(binding) {
-            btEdit.setOnClickListener {
-                if(status) {
-                    tvName.visibility = View.VISIBLE
-                    status = false
-                } else {
-                    tvName.visibility = View.GONE
-                    status = true
-                }
-            }
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
         }
 
-    }
+        binding?.run {
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main_menu, menu)
-        return super.onCreateOptionsMenu(menu)
+        }
+
     }
 }
